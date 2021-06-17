6f66c05b1b7661a5c6c7d39f8fa3bb1b6f3590ba
yushan <babycat52033@gmail.com>
2021-06-18 01:52:01 +0800

    Read/Write File

--- account.py
+++ account.py
@@ -1,13 +1,34 @@
 #Accounting
 products = []
+#Read file
+with open ("product.csv", "w", encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for line in f:
+		if '商品, 價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+#User input
 while True:
 	name = input ("請輸入商品名稱： ")
 	if name == 'q':
 		break
-	price = input ("請輸入商品價格： ")
+	price = int(input ("請輸入商品價格： "))
 	p = []
 	p.append(name)
 	p.append(price)
 	products.append(p)
 
 print(products)
+
+#Print
+for p in products:
+	print(p[0], '的價格', p[1])
+
+#Write file
+with open ("product.csv", "w", encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) +'\n')
+		 
